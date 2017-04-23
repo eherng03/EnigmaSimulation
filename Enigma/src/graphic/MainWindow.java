5562d1150706212eae4d23b1caa195772e055e8d
EvaHergar <eherng03@estudiantes.unileon.es>
2017-04-24 00:06:39 +0200

    Enigma graphic interface

---
+++ Enigma/src/graphic/MainWindow.java
@@ -0,0 +1,68 @@
+package graphic;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import java.awt.Color;
+import java.awt.SystemColor;
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class MainWindow {
+
+	private JFrame frame;
+	private JPanel image;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainWindow window = new MainWindow();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public MainWindow() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.getContentPane().setBackground(new Color(102, 55, 0));
+		frame.setBounds(100, 100, 450, 362);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		image = new EnigmaImage(414, 239);
+		image.setBounds(10, 11, 414, 239);
+		frame.getContentPane().add(image);
+		
+		JButton btnCifrarMensaje = new JButton("Cifrar mensaje");
+		btnCifrarMensaje.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				//abrir fichero con el mensaje
+				//llamar a cifrador
+				//preguntar donde guardarlo
+			}
+		});
+		btnCifrarMensaje.setBackground(SystemColor.activeCaptionBorder);
+		btnCifrarMensaje.setFont(new Font("Candara", Font.BOLD, 18));
+		btnCifrarMensaje.setBounds(10, 261, 414, 47);
+		frame.getContentPane().add(btnCifrarMensaje);
+	}
+}
