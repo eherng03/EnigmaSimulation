829074ec99f14bb8aea7e9d372ec3de855e5b2b3
EvaHergar <eherng03@estudiantes.unileon.es>
2017-04-27 21:02:39 +0200

    Update Rotor.java

--- Enigma/src/logic/Rotor.java
+++ Enigma/src/logic/Rotor.java
@@ -5,13 +5,21 @@ public class Rotor {
 	private int initPosition;
 	private int currentPosition;
 	private String[] letters;
+	private int[] abecedarioNumerico;
 	
 	public Rotor(String[] letters) {
 		this.letters = letters;
+		for(int i = 0; i < letters.length; i++){
+			abecedarioNumerico[i] = letters[i].toCharArray()[0] - 'A'; 		//Le restamos 65 para que este en el rango de 0 a 
+		}
 	}
 	
 	public void setInitPosition(char initPosition){
-		
+		for(int i = 0; i < letters.length; i++){
+			if(letters[i].equals(initPosition)){
+				
+			}
+		}
 		//TODO cambiar, buscar el char en el alfabeto
 		this.initPosition = initPosition;
 		currentPosition = initPosition;
